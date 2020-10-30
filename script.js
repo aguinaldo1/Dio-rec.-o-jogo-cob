405f8cdf11e4f614d59dfa90d6a4116516aca884
unknown <aguinaldoamerico2@gmail.com>
2020-10-30 12:13:23 -0300

    definir movimento da cobrinha

--- script.js
+++ script.js
@@ -2,42 +2,27 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-snake [0] = {
+snake[0] = {
     x: 8 * box,
-    y: 8 * box,
-
+    y: 8 * box
 }
-
 let direction = "right";
 
 function criarBG(){
-    context.fillStyle="lightgreen";
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
-for(i=0; i < snake.length; i++){
-    context.fillStyle = "green";
-    context.fillRect(snake[i].x, snake[i].y, box, box);
-  }
-}
-
-document.addEventListener('keydown',update);
-
-function update (evente){
-    if(evente.keyCode == 37 && direction !="right") direction = "left";
-    if(evente.keyCode == 38 && direction !="down") direction = "up";
-    if(evente.keyCode == 39 && direction != "left") direction = "right";
-    if(evente.keyCode == 40 && direction != "up") direction = "down";
-
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box,box);
+    }
+    
 }
 
 function iniciarJogo(){
-    if(snake[0]. x > 15 * box && direction == "right") snake[0].x=0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y=0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y =16 * box;
-    
+
     criarBG();
     criarCobrinha();
 
@@ -51,7 +36,7 @@ function iniciarJogo(){
 
     snake.pop();
 
-    let newHead = {
+    let newHead =  {
         x: snakeX,
         y: snakeY
     }
@@ -59,4 +44,5 @@ function iniciarJogo(){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarjogo, 100);
+let joto = setInterval(iniciarJogo, 100);
+
